3f4390022171dfffbcfed1da32c58720da9c16d9
Ashhal Idrees <129538410+itsAshhal@users.noreply.github.com>
2024-08-23 17:39:07 +0500

    Last Update
    Complete

--- Assets/Scripts/Localization/LocalizeText.cs
+++ Assets/Scripts/Localization/LocalizeText.cs
@@ -31,9 +31,21 @@ namespace Econagri.Localization
         public void OnChangeLanguageMethod(int localeId)
         {
             Debug.Log($"Translation called");
+            StartCoroutine(ChangeTextCoroutine(localeId));
 
+        }
+
+        IEnumerator ChangeTextCoroutine(int localeId)
+        {
             // change the fonts for each language as well
+            TMP_Text textComponent = GetComponent<TMP_Text>();
+            string text = textComponent.text;
+            //textComponent.text = "";
+            GetComponent<Animator>().CrossFade("Disappear", .1f);
+            yield return new WaitForSeconds(.5f);
             GetComponent<TMP_Text>().font = localeId == 0 ? _Font.EnglishFont : _Font.HindiFont;
+            //textComponent.text = text;
+            GetComponent<Animator>().CrossFade("Appear", .1f);
         }
 
     }
